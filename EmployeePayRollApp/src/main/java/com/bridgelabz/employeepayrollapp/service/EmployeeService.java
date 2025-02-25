46bd80478e6eb75a6dcff622bf6ebef225a29b05
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-02-25 11:49:21 +0530

    [Mohit Soni] Add. Ability for the Services Layer to store the Employee Payroll Data

--- EmployeePayRollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
+++ EmployeePayRollApp/src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeeService.java
@@ -1,3 +1,4 @@
+
 package com.bridgelabz.employeepayrollapp.service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface EmployeeService {
     Employee createEmployee(EmployeeDTO employeeDTO);
     List<Employee> getAllEmployees();
+    Employee updateEmployee(Long id, EmployeeDTO employeeDTO);
+    void deleteEmployee(Long id);
 }
